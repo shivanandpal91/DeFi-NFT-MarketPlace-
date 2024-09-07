4f06bc89d9f7f3127396a9bac92a0aa5e14c77ca
Shivanand Pal <palmanish200@gmail.com>
2024-09-07 22:06:53 +0530

    update

--- migrations/2_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-
-const DefiMarketplace = artifacts.require('DefiMarketplace')
-
-module.exports = async (deployer) => {
-  const accounts = await web3.eth.getAccounts()
-
-  await deployer.deploy(DefiMarketplace, 'Defi Marketplace NFTs', 'DMNFT', 10, accounts[1])
-}
